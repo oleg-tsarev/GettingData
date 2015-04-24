ab527fd066c3b8adb6d822c2ae299219d49ebb2d
oleg <oleg_tsarev@mail.ru>
2015-04-24 23:42:29 +0300

    update run_analysis + slightly changes

--- run_analisys.R
+++ run_analisys.R
@@ -1,13 +1,13 @@
 # === script preparation === #
-setwd("~/Documents/GettingData/project")
+setwd("~/Documents/3 - GettingData/project")
 rm(list=ls())
 
 
 # === download and unzip data folder === #
-#download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-#              ,"~/Documents/GettingData/project/Dataset.zip",method = "wget")
-#unzip(zipfile = "~/Documents/GettingData/project/Dataset.zip",overwrite = T,
-#      exdir = "~/Documents/GettingData/project/")
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+              ,"~/Documents/3 - GettingData/project/Dataset.zip",method = "wget")
+unzip(zipfile = "~/Documents/3 - GettingData/project/Dataset.zip",overwrite = T,
+      exdir = "~/Documents/3 - GettingData/project/")
 
 
 # === test processing === #
@@ -19,13 +19,13 @@ right <- read.table("UCI HAR Dataset/test/X_test.txt",header=F)
 
 left <- as.data.frame(cbind(subject[1],activity_code[1]))
 names(left) <- c("subject","activity_code")
-left <- merge(left,activity_labels,by.x="activity_code",by.y="V1",all=T)
+left <- merge(left,activity_labels,by.x="activity_code",by.y="V1")
 names(left)[3] <- "activity_label"
 left$activity_label <- as.factor(left$activity_label)
 left <- left[,c(2,1,3)]
 names(right) <- features$V2
 right <- right[,grep("mean\\(|std\\(",names(right),value=TRUE)]
-names(right) <- tolower(gsub("-","",gsub("\\()","",names(right))))
+names(right) <- tolower(gsub("-",".",gsub("\\()","",names(right))))
 test <- cbind(left,right)
 
 
@@ -38,12 +38,12 @@ right <- read.table("UCI HAR Dataset/train/X_train.txt",header=F)
 
 left <- as.data.frame(cbind(subject[1],activity_code[1]))
 names(left) <- c("subject","activity_code")
-left <- merge(left,activity_labels,by.x="activity_code",by.y="V1",all=T)
+left <- merge(left,activity_labels,by.x="activity_code",by.y="V1")
 names(left)[3] <- "activity_label"
 left <- left[,c(2,1,3)]
 names(right) <- features$V2
 right <- right[,grep("mean\\(|std\\(",names(right),value=TRUE)]
-names(right) <- tolower(gsub("-","",gsub("\\()","",names(right))))
+names(right) <- tolower(gsub("-",".",gsub("\\()","",names(right))))
 train <- cbind(left,right)
 dataset <- rbind(test,train)
 
@@ -52,7 +52,7 @@ dataset <- rbind(test,train)
 tidy <- dataset[,colnames(dataset) != "activity_code"]
 tidy <- aggregate(tidy,by=list(tidy$subject,tidy$activity_label),FUN=mean,na.rm=T)
 tidy <- tidy[,!(colnames(tidy) %in% c("subject","activity_label"))]
-names(tidy)[1:2] <- c("activity_label","subject")
+names(tidy)[1:2] <- c("subject","activity_label")
 
 # === export to txt === #
 rm(list=ls()[! ls() %in% c("dataset","tidy")])
